5d0fb2f72a65f50db86f96befb6dfcaab56c0164
Vitor <vitorafael.r.c@gmail.com>
2021-10-13 18:32:49 -0300

    delete user

--- server.py
+++ server.py
@@ -88,11 +88,55 @@ class Server(http.server.SimpleHTTPRequestHandler):
             copy = old_archives.copy()
             for idx, user in enumerate(copy):
                 if user["registration"] == post_data["registration"]:
-                    self.send_response(202)
+                    self.send_response(200)
                     self.send_header("Content-type", "application/json")
                     self.end_headers()
                     
                     copy[idx] = post_data
+
+                    db = open("text.txt", "w")
+                    db.writelines(json.dumps(copy))
+                    db.close()
+                    
+                    res = {
+                        "data": post_data,
+                        "status": "success"
+                    }
+                    
+                    return self.wfile.write(json.dumps(res).encode())
+            db.close()
+            self.send_response(404)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            res = {
+                "data": "",
+                "status": "not found"
+            }
+            return self.wfile.write(json.dumps(res).encode())
+    
+    def do_DELETE(self):
+        if self.path == '/users':
+            content_length = int(self.headers['Content-Length']) 
+            post_data = json.loads(self.rfile.read(content_length))
+            db = open("text.txt", "r")
+
+            old_archives = db.readlines()
+            if not old_archives:
+                old_archives = []
+            else:
+                all_text = ""
+                for text in old_archives:
+                    all_text += text
+                old_archives = json.loads(all_text)
+
+            copy = old_archives.copy()
+            for idx, user in enumerate(copy):
+                if user["registration"] == post_data["registration"]:
+                    self.send_response(200)
+                    self.send_header("Content-type", "application/json")
+                    self.end_headers()
+                    
+                    del(copy[idx])
 
                     db = open("text.txt", "w")
                     db.writelines(json.dumps(copy))
